72b47beee7c66cdf09bdc73ed4e3a6e8cf452393
Tetsewa <amatwilliams@gmail.com>
2024-09-16 00:58:08 +0200

    updated with images and About

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -17,7 +17,7 @@ const About: React.FC = () => {
         <h1 className="text-4xl font-bold mb-4 text-gray-800">About Me</h1>
         <p className="text-lg text-gray-700 mb-4">
           Hi! I'm <strong>Ama Williams</strong>, a passionate web developer dedicated to creating seamless, responsive, and visually stunning web experiences. 
-          With a strong foundation in HTML, CSS, JavaScript, and frameworks like <strong>[MERN stack and JavaScript]</strong>, I specialize in crafting websites and web applications 
+          With a strong foundation in HTML, CSS, JavaScript, and frameworks like <strong>MERN stack and JavaScript</strong>, I specialize in crafting websites and web applications 
           that prioritize both functionality and user experience.
         </p>
         <p className="text-lg text-gray-700 mb-4">
@@ -25,7 +25,7 @@ const About: React.FC = () => {
           My portfolio reflects my journey in building projects that range from games and management applications to dynamic web applications and e-commerce platforms.
         </p>
         <p className="text-lg text-gray-700 mb-4">
-          When I'm not coding, I enjoy <strong>[listening to music, singing, and baking]</strong>, which keeps me creative and inspired.
+          When I'm not coding, I enjoy <strong>listening to music, singing, and baking</strong>, which keeps me creative and inspired.
         </p>
         <p className="text-lg text-gray-700">
           Feel free to explore my projects and get in touch if you'd like to collaborate or learn more about my work!
